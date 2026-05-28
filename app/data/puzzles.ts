9e4ee7a9254a640ca36bff43434bd5b294ddc115
Deven <devenyvandenberg@gmail.com>
2026-05-28 11:25:26 +0200

    eerste versie woordgreep

---
+++ app/data/puzzles.ts
@@ -0,0 +1,27 @@
+export const puzzles = [
+  {
+    id: 1,
+    clue: "Dronken arts is stijf. (4)",
+    answer: "star",
+    hints: [
+      "Indicator: dronken",
+      "Bouwsteen: arts",
+      "Definitie: stijf",
+    ],
+    explanation:
+      "‘Stijf’ is de definitie. ‘Dronken’ is de indicator: die zegt dat je de letters moet husselen. ‘Arts’ zijn de bouwstenen. ARTS wordt STAR.",
+  },
+
+  {
+    id: 2,
+    clue: "Naar verluid is Leiden er slecht aan toe. (5)",
+    answer: "lijden",
+    hints: [
+      "Indicator: naar verluid",
+      "Bouwstenen: Leiden",
+      "Definitie: slecht eraan toe",
+    ],
+    explanation:
+      "‘Naar verluid’ geeft aan dat het om een homophone gaat. Leiden klinkt als lijden.",
+  },
+];
