3970f05de8f5fe41f757a8c2f057f4745b4f4186
Deven <devenyvandenberg@gmail.com>
2026-07-03 00:08:05 +0200

    puzzle0307

--- app/data/puzzles.ts
+++ app/data/puzzles.ts
@@ -788,7 +788,7 @@ export const puzzles = [
     hints: { 
 
       definitie: ["Huilbui"], 
-, 
+      
       indicatoren: ["vangt", "gekke", "zonder", "einde"],  
 
       bouwstenen: ["aan", "start", "dwaas"], 
