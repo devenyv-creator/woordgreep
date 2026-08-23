ea677d249204d832f1c74370ea4bdf7eac7d4b80
Deven <devenyvandenberg@gmail.com>
2026-08-23 23:03:18 +0200

    puzzles2408

--- app/data/puzzles.ts
+++ app/data/puzzles.ts
@@ -2007,7 +2007,31 @@ credit: "💜 Ingestuurd door Max",
 
 }, 
 
- 
+   
+
+     { 
+
+    date: "2026-08-24", 
+
+    clue: "Max, 1 keer, niet meer. (8)", 
+
+    answer: "maximaal", 
+
+    hints: { 
+
+      definitie: ["niet meer"], 
+
+      indicatoren: [],  
+
+      bouwstenen: ["1", "keer"], 
+
+    }, 
+
+    explanation: 
+
+      "Deze puzzel heeft geen indicatoren, wat betekent dat het antwoord het in principe al staat. Je ziet alleen dat het er nog niet staat, dus je moet wel nog iets doen. Er is één type actie die je altijd met bouwstenen mag doen, namelijk substities. Het woord 'max' houden we zo, we gaan het cijfer 1 substitueren voor het Romeinse cijfer 'i' en we gaan 'keer' vervangen voor een synoniem, namelijk 'maal'. Als je dit achter elkaar zet, krijg je 'maximaal' en dit past bij de definitie 'niet meer'.",  
+
+}, 
  
 
 ]
